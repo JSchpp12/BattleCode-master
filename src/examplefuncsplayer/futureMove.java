f883a812b727c32b59cc3f61f51a3942b230c3dd
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2020-01-12 16:18:32 -0500

    set up futureMove structure to store future moves

---
+++ src/examplefuncsplayer/futureMove.java
@@ -0,0 +1,18 @@
+//this class is a container to hold future moves and the next move
+package examplefuncsplayer;
+import java.util.*;
+
+public class futureMove {
+    private int[] moveList; //array to contain the future moves
+    private int nextMove = 0;
+
+    public futureMove(int[] inMoveList) {
+        this.moveList = inMoveList;
+    }
+
+    public int getNextMove() {
+        int returnMove = this.moveList[nextMove];
+        this.nextMove++;
+        return returnMove;
+    }
+}
